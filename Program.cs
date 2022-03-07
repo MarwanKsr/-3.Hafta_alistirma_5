49e2d3301fa33be78dfbe09606ddae0fdab405e9
Marwan <Marwan.kaseer1997@gmail.com>
2022-03-08 01:43:46 +0300

    Program is done

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _3.Hafta_alistirma_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("sayiyi giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sonuc = 0;
+            for (int i=1; i<=200; i++)
+            {
+                if (i%sayi == 0)
+                {
+                    sonuc++;
+                }
+                
+            }
+            Console.WriteLine("1-200 arasinda {0} sayinin bolunenlerinin sayisi {1}",sayi,sonuc);
+         }
+    }
+}
